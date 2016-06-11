29c65968ff7cd79b2c83a08d46c477877e19fe4a
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2016-06-11 16:06:19 +0300

    Fixed some production issues (fonts still need fixing)

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -27,7 +27,7 @@ module.exports = {
     path: assetsPath,
     filename: '[name]-[chunkhash].js',
     chunkFilename: '[name]-[chunkhash].js',
-    publicPath: '/dist/'
+    publicPath: 'dist/'
   },
   module: {
     loaders: [
